ba039a446f403979a8d9fcb09c18612abd34bef7
sekibaalamark <markmajje256@gmail.com>
2025-07-18 15:27:20 +0300

    corrected the serial output

--- sekibaala Mark/smart_street_lights.cpp
+++ sekibaala Mark/smart_street_lights.cpp
@@ -3,7 +3,7 @@
 #include <Wire.h>
 
 // Pin definitions
-#define LDR_PIN 36          // LDR voltage divider connected to GPIO 36 (A0)
+#define LDR_PIN 34          // LDR voltage divider connected to GPIO 36 (A0)
 #define LED_PIN 2           // LED connected to digital pin 2
 #define SDA_PIN 21          // I2C SDA pin for RTC
 #define SCL_PIN 22         // I2C SCL pin for RTC
@@ -56,10 +56,6 @@ void setup() {
   }
   
   Serial.println("ESP32 Smart Light Control System Initialized");
-  //Serial.println("LED will turn on if:");
-  //Serial.println("1. It's nighttime (6PM-6AM) AND it's dark");
-  //Serial.println("2. It's extremely dark during daytime");
-  //Serial.println("Use potentiometer to adjust LDR sensitivity in real-time");
 }
 
 void loop() {
@@ -79,10 +75,7 @@ void loop() {
     if(currentHour >= nightStartHour && currentHour <=nightEndHour){
       isNightTime = true;
     }
-  }// else {
-    // Night doesn't span midnight
-   // isNightTime = (currentHour >= nightStartHour && currentHour < nightEndHour);
-  //}
+  }
   
   // Determine if LED should be on
   bool shouldTurnOnLED = false;
